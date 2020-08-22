c39c604c14e1042f119747fefd3bea8588b51e71
isakwaldener <isak_waldener@hotmail.com>
2020-08-22 22:22:39 +0200

    Update GUI.py

--- Client/GUI.py
+++ Client/GUI.py
@@ -1,9 +1,9 @@
 import tkinter
 from Enter_server import Enter_server
+from message import message_frame
 
 class GUI:
 
-    msg_frame = None
     root = tkinter.Tk()
 
 
@@ -22,49 +22,13 @@ class GUI:
     def root_quit(self):
         self.root.destroy()
 
-    def entry_field_msg_frame(self):
-        entry = tkinter.Entry(self.msg_frame)
-        entry.pack()
-        return entry
-    
-    def destroy_msg_frame(self):
-        self.msg_frame.destroy()
-    
-    def initialize_frame(self, frame):
-        frame = tkinter.Frame(self.root)
-    
-    def message_window_frame(self):
-
-        self.msg_frame = tkinter.Frame(self.root)
-        self.client.message = tkinter.StringVar()
-        self.client.set_message("Enter message here")
-        scrollbar = tkinter.Scrollbar(self.msg_frame)
-        self.client.set_message_list(tkinter.Listbox(self.msg_frame, height=10, 
-                                     width=50, yscrollcommand=scrollbar))
-        scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-        self.client.msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-        self.client.msg_list.pack()
-        self.msg_frame.pack()
-
-        entry_field = tkinter.Entry(self.root,
-                                    textvariable=self.client.message)
-        entry_field.bind("<Return>", self.client.send)
-        entry_field.pack()
-        send_button = tkinter.Button(self.root, text="Send", 
-                                     command=self.client.send)
-        send_button.pack()
-
     def join_server(self, host, port):
         self.enter_server.destroy_enter_server_frame()
-        self.message_window_frame()
+        self.create_message_frame()
         self.client.setADDR(host, port)
         self.client.set_socket()
         self.client.create_thread()
-        
+    
+    def create_message_frame(self):
+        msg_frame = message_frame(self, self.client)
 
-    def select_chat_room(self):
-        button1 = Button(self.root, text="25 min timer")
-        button1.pack()
-
-        button2 = Button(self.root, text="5 min break")
-        button2.pack()
