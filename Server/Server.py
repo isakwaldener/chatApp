d1581c66ae43e53dcad7ba13380f015dfb7304d7
isakwaldener <isak_waldener@hotmail.com>
2020-08-24 20:36:47 +0200

    Update Server.py
    added more so u can join up to 50 different chat rooms

--- Server/Server.py
+++ Server/Server.py
@@ -4,6 +4,7 @@ from threading import Thread
 """VARIABLES"""
 clients = []
 addresses = []
+rooms = []
 
 """ Constants """
 HOST = ''
@@ -26,29 +27,54 @@ def accept_connection():
 def process_client(client):
 
     name = client.recv(BUFSIZE).decode("utf8")
+    room_number = select_chat_room(client)
     welcome = f"Welcome {name}! if you want to quit type (QUIT)"
     client.send(bytes(welcome, "utf8"))
     msg = f"{name} has joined the chat"
-    broadcast(bytes(msg, "utf8"))
-    clients.append(client)  # maybe need to save name as well
+    broadcast(bytes(msg, "utf8"), room_number)
 
     while True:
         msg = client.recv(BUFSIZE)
         if msg != bytes("(QUIT)", "utf8"):
-            broadcast(msg, f"{name}: ")
+            broadcast(msg, room_number, f"{name}: ")
         else:
             client.send(bytes("(QUIT)", "utf8"))
             client.close()
-            clients.remove(client)
-            broadcast(bytes(f"{name} left the chat", "utf8"))
-            print(f"{name} left the chat")
+            remove_client_from_room(client, room_number)
+            broadcast(bytes(f"{name} left the chat", "utf8"), room_number)
             break
 
+def select_chat_room(client):
+    client.send(bytes("select chat room", "utf8"))
+    room_number = int(client.recv(BUFSIZE).decode("utf8"))
+    add_client_to_chat_room(client, room_number)
+    return room_number
 
-def broadcast(msg, prefix=""):
-    for client in clients:
+def add_client_to_chat_room(client, room_number):
+    room = get_room(room_number)
+    room.append(client)
+
+def remove_client_from_room(client, room_number):
+      room = get_room(room_number) 
+      room.remove(client)
+
+def get_room(room_number):
+    if len(rooms) > room_number:
+        return rooms[room_number]
+    else:
+        return create_room_and_return_it(room_number)
+
+def create_room_and_return_it(room_number):
+    while room_number >= len(rooms):
+        rooms.append([])
+        if len(rooms) = 50:
+            break
+    return rooms[-1]
+
+def broadcast(msg, room_number, prefix=""):
+    for client in rooms[room_number]:
         client.send(bytes(prefix, "utf8") + msg)
-
+            
 
 if __name__ == "__main__":
     SERVER.listen(5)
